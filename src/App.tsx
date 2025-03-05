594f1168ceb435de252e89dfda36bca689efb355
Mia Batista <miajolie@bu.edu>
2025-03-05 14:02:34 -0500

    starting the project and i thin i added all the necessary routes in app.tsx

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import {Routes, Route, createBrowserRouter, RouterProvider} from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Nav from "./components/Nav";
+import Education from "./components/Education";
+import Skills from "./components/Skills"
+import WorkExperience from "./components/WorkExperience";
+import ActivitiesHobbies from "./components/ActivitiesHobbies";
+import Projects from "./components/Projects";
+
+
+function Root() {
+  return (
+    <div>
+      <Header/>
+      <Nav/>
+      <Routes>
+        <Route path= {`/`} element = {<Home/>}/>
+        <Route path= {`/Education`} element = {<Education/>}/>
+        <Route path= {`/Skills`} element = {<Skills/>}/>
+        <Route path= {`/WorkExperience`} element = {<WorkExperience/>}/>
+        <Route path= {`/ActivitiesHobbies`} element = {<ActivitiesHobbies/>}/>
+        <Route path= {`/Projects`} element = {<Projects/>}/>
+      </Routes>
+    </div>
+  );
+}
+
+const router = createBrowserRouter([{path : "*", Component: Root}])
+
+export default function App() {
+  return <RouterProvider router = {router}/>
+}
